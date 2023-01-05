713b61e74860f11f0de49810003efe28d14fdcd2
Shadi Mostafa <shadym@gmail.com>
2023-01-05 23:21:07 +0300

    Update detectReflection.ino
    changed to library esp32cam.h and adjusted code accordingly

--- detectReflection.ino
+++ detectReflection.ino
@@ -1,11 +1,10 @@
-#include <esp_camera.h>
 #include <WiFi.h>
+#include <esp32cam.h>
 
 const char* ssid = "YOUR_WIFI_SSID";
 const char* password = "YOUR_WIFI_PASSWORD";
 
-// Create a global variable to store the camera configuration
-camera_config_t config;
+esp32CAM config;
 
 void setup() {
   // Initialize the serial port
@@ -21,32 +20,16 @@ void setup() {
   Serial.println("WiFi connected");
 
   // Initialize the camera module and set the configuration
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_QVGA;
-  config.jpeg_quality = 10;
-  config.fb_count = 1;
+  config.setPin(CAM_PIN_D0, CAM_PIN_D1, CAM_PIN_D2, CAM_PIN_D3, CAM_PIN_D4, CAM_PIN_D5, CAM_PIN_D6, CAM_PIN_D7, CAM_PIN_XCLK, CAM_PIN_PCLK, CAM_PIN_VSYNC, CAM_PIN_HREF, CAM_PIN_SIOD, CAM_PIN_SIOC, CAM_PIN_PWDN, CAM_PIN_RESET);
+  config.setXCLKFreq(20000000);
+  config.setPixelFormat(PIXFORMAT_JPEG);
+  config.setFrameSize(FRAMES
+  config.setFrameSize(FRAMESIZE_QVGA);
+  config.setJPEGQuality(10);
+  config.setFBCount(1);
 
   // Initialize the camera module
-  esp_err_t err = esp_camera_init(&config);
+  esp_err_t err = config.begin();
   if (err != ESP_OK) {
     Serial.println("Error initializing camera module");
     return;
@@ -55,7 +38,7 @@ void setup() {
 
 void loop() {
   // Capture an image from the camera module
-  camera_fb_t* fb = esp_camera_fb_get();
+  camera_fb_t* fb = config.getFrame();
   if (!fb) {
     Serial.println("Error capturing image");
     return;
@@ -64,58 +47,57 @@ void loop() {
   // Convert the image to grayscale
   uint8_t* gray = (uint8_t*) malloc(fb->width * fb->height);
   for (int i = 0; i < fb->width * fb->height; i++) {
-gray[i] = (fb->buf[i * 3] + fb->buf[i * 3 + 1] + fb->buf[i * 3 + 2]) / 3;
-}
+    gray[i] = (fb->buf[i * 3] + fb->buf[i * 3 + 1] + fb->buf[i * 3 + 2]) / 3;
+  }
 
-// Apply a high-pass filter to the grayscale image to highlight areas that are significantly brighter or darker than their surroundings
-uint8_t* high_pass = (uint8_t*) malloc(fb->width * fb->height);
-for (int i = 0; i < fb->width * fb->height; i++) {
-if (i == 0) {
-high_pass[i] = abs(gray[i] - gray[i + 1]);
-} else if (i == fb->width * fb->height - 1) {
-high_pass[i] = abs(gray[i] - gray[i - 1]);
-} else {
-high_pass[i] = abs(gray[i] - (gray[i - 1] + gray[i + 1]) / 2);
-}
-}
+  // Apply a high-pass filter to the grayscale image to highlight areas that are significantly brighter or darker than their surroundings
+  uint8_t* high_pass = (uint8_t*) malloc(fb->width * fb->height);
+  for (int i = 0; i < fb->width * fb->height; i++) {
+    if (i == 0) {
+      high_pass[i] = abs
+  high_pass[i] = abs(gray[i] - gray[i + 1]);
+    } else if (i == fb->width * fb->height - 1) {
+      high_pass[i] = abs(gray[i] - gray[i - 1]);
+    } else {
+      high_pass[i] = abs(gray[i] - (gray[i - 1] + gray[i + 1]) / 2);
+    }
+  }
 
-// Use an edge detection algorithm to identify the boundaries of reflective regions in the image
-uint8_t* edges = (uint8_t*) malloc(fb->width * fb->height);
-for (int i = 0; i < fb->width * fb->height; i++) {
-if (i == 0 || i == fb->width * fb->height - 1) {
-edges[i] = 0;
-} else {
-int gradient = abs(high_pass[i] - high_pass[i - 1]) + abs(high_pass[i] - high_pass[i + 1]);
-if (gradient > 50) {
-edges[i] = 255;
-} else {
-edges[i] = 0;
+  // Use an edge detection algorithm to identify the boundaries of reflective regions in the image
+  uint8_t* edges = (uint8_t*) malloc(fb->width * fb->height);
+  for (int i = 0; i < fb->width * fb->height; i++) {
+    if (i == 0 || i == fb->width * fb->height - 1) {
+      edges[i] = 0;
+    } else {
+      int gradient = abs(high_pass[i] - high_pass[i - 1]) + abs(high_pass[i] - high_pass[i + 1]);
+      if (gradient > 50) {
+        edges[i] = 255;
+      } else {
+        edges[i] = 0;
+      }
+    }
+  }
+
+  // Count the number of reflective pixels in the image
+  int reflection_count = 0;
+  for (int i = 0; i < fb->width * fb->height; i++) {
+    if (edges[i] == 255) {
+      reflection_count++;
+    }
+  }
+
+  // Print the result to the serial port
+  Serial.print("Reflection count: ");
+  Serial.println(reflection_count);
+
+  // Free the memory allocated for the image buffers
+  free(gray);
+  free(high_pass);
+  free(edges);
+
+  // Release the camera frame buffer
+  config.returnFrame(fb);
+
+  // Delay for 1 second before capturing the next image
+  delay(1000);
 }
-}
-}
-
-// Count the number of reflective pixels in the image
-int reflection_count = 0;
-for (int i = 0; i < fb->width * fb->height; i++) {
-if (edges[i] == 255) {
-reflection_count++;
-}
-}
-
-// Print the result to the serial port
-Serial.print("Reflection count: ");
-Serial.println(reflection_count);
-
-// Free the memory allocated for the image buffers
-free(gray);
-free(high_pass);
-free(edges);
-
-// Release the camera frame buffer
-esp_camera_fb_return(fb);
-
-// Delay for 1 second before capturing the next image
-delay(1000);
-}
-
-
